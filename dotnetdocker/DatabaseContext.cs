835b55b4a0db6053ad94be7af215eaad6af9fd80
elronyazilim <55488502+elronyazilim@users.noreply.github.com>
2020-02-17 09:20:28 +0300

    first commit

---
+++ dotnetdocker/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetdocker
+{
+    public class DatabaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=tcp:noyaxsrv.database.windows.net,1433;Database=noyaxdb;User ID=elronsa;Password=@lr0nsa!;Encrypt=true;Connection Timeout=30;");
+        }
+
+        public DbSet<Messages> Messages { get; set; }
+    }
+}
---
+++ dotnetdocker/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetdocker
+{
+    public class Messages
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+    }
+}
